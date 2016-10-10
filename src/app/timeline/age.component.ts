3526c4b7a5918b2a3ede5e40ecc5e278c7811043
Craig Burton <cburton@platformq.com>
2016-10-09 22:09:34 -0400

    Added original animations to timeline page

--- src/app/timeline/age.component.ts
+++ src/app/timeline/age.component.ts
@@ -1,16 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChildren, QueryList, AfterViewInit, HostListener } from '@angular/core';
 
 import { Age } from './age';
+
+import { Observable } from 'rxjs/Observable';
+
+import { BlockComponent } from './block.component.ts';
 
 @Component({
     selector: 'timeline-age',
     templateUrl: './age.component.html'
 })
-export class AgeComponent {
+export class AgeComponent implements AfterViewInit {
     @Input() age: Age;
+
+    @ViewChildren( BlockComponent )
+    private children: QueryList<BlockComponent>;
+
+    private scrollEvents: Observable<any>;
+
+    constructor(){
+        this.scrollEvents = Observable.fromEvent(window, 'scroll');
+        this.scrollEvents.subscribe(()=> this.handleScroll());
+    }
+
     title: string = 'Snap!';
 
     logIt(age: Age){
         console.log(age);
     }
+
+    getScrollOffset(): number {
+        let doc = document.documentElement;
+        let windowOffset = ( window.pageYOffset || doc.scrollTop ) - ( doc.clientTop || 0 );
+        return windowOffset + (window.innerHeight * 0.5);
+    }
+
+    handleScroll(): void {
+      window.requestAnimationFrame(() => {
+        let offset = this.getScrollOffset();
+        this.children.forEach((child: BlockComponent)=> {
+            child.handleScroll(offset);
+        });
+      });
+    }
+
+    ngAfterViewInit(): void {
+        this.handleScroll();
+    }
+
+
 }
--- src/app/timeline/block.component.ts
+++ src/app/timeline/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, HostListener, ElementRef } from '@angular/core';
 
 import { Block } from './block';
 
@@ -8,4 +8,30 @@ import { Block } from './block';
 })
 export class BlockComponent {
     @Input() block: Block;
+    shown: boolean = false;
+    $el: any;
+
+    constructor(private element: ElementRef){
+        this.$el = element.nativeElement;
+    }
+
+    getClasses(isImg: boolean): {'is-hidden': boolean, 'bounce-in': boolean} {
+        let classes = {'is-hidden': !this.shown, 'bounce-in': this.shown};
+        if(isImg){
+          classes[this.block.type] = true;
+        }
+        return classes;
+    }
+
+    handleScroll(scrollOffset: number): void{
+        if(this.shown){
+            return;
+        }
+        let self = this;
+
+        if(scrollOffset >= this.$el.offsetTop){
+            self.shown = true;
+        }
+    }
+
 }
